a41f5c4a82bb0d11c01f48dc114ecad628221e6e
bitar011 <victorbitarr@gmail.com>
2025-04-01 09:38:06 -0300

    functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function adicionar(valor) {
+    document.getElementById('display').value += valor;
+}
+
+function limpar() {
+    document.getElementById('display').value = '';
+}
+
+function calcular() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (e) {
+        alert('Erro na expressão!');
+        limpar();
+    }
+}
